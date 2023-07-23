f50a5d8acf87eb147fba4bb9f35570d2980c1072
Ryan Weiss <ryan-aaron.weiss@mohawkcollege.ca>
2023-07-22 22:04:13 -0400

    UI file size calculating/readme new preview image

--- src/App.js
+++ src/App.js
@@ -231,12 +231,25 @@ function App()
                 </td>
                 <td className="whitespace-nowrap text-sm text-gray-500">
                   {file.thumbnail ? <img src={file.thumbnail} alt={file.name} className="w-8 h-8 rounded-md"/>
+                    : file.type == 'txt' ? <i className="bi bi-file-text text-2xl"></i>
+                    : file.type == 'pdf' ? <i className="bi bi-file-earmark-pdf text-2xl"></i>
+                    : file.type == 'doc' || file.type == 'docx' ? <i className="bi bi-file-earmark-word text-2xl"></i>
+                    : file.type == 'mkv' || file.type == 'mp4' || file.type == 'avi' || file.type == 'mov' ? <i className="bi bi-file-earmark-play text-2xl"></i>
+                    : file.type == 'mp3' || file.type == 'wav' || file.type == 'ogg' || file.type == 'flac' ? <i className="bi bi-file-earmark-music text-2xl"></i>
+                    : file.type == 'zip' || file.type == 'rar' || file.type == '7z' ? <i className="bi bi-file-earmark-zip text-2xl"></i>
+                    : file.type == 'exe' ? <i className="bi bi-file-earmark-binary text-2xl"></i>
                     : <i className="bi bi-file-earmark-text text-3xl"></i>
                   }
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{file.name}</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 uppercase tracking-wider">{file.type}</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{(file.size / 1048576).toFixed(2)} MB</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  {file.size < 1024 ? `${file.size} B`
+                    : file.size < 1048576 ? `${(file.size / 1024).toFixed(2)} KB`
+                    : file.size < 1073741824 ? `${(file.size / 1048576).toFixed(2)} MB`
+                    : `${(file.size / 1073741824).toFixed(2)} GB`
+                  }
+                </td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{new Date(file.date).toLocaleString()}</td>
               </tr>
             )) : (
