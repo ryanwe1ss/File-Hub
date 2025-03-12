1dd2cecb6bf09912ceb848a54105e07eabcc2132
ryan <ryanweiss02@gmail.com>
2025-03-11 22:34:15 -0400

    better ui for authentication prompt

--- src/components/auth-modal.js
+++ src/components/auth-modal.js
@@ -1,12 +1,23 @@
-import { useRef } from 'react';
+import { useState } from 'react';
 import '../css/auth-modal.scss';
 
 function AuthenticationModal(args)
 {
-  const passwordRef = useRef(null);
-  const messageRef = useRef(null);
+  const [request, setRequest] = useState({
+    message: 'Authenticate',
+    authenticating: false,
+    password: null,
+  });
+
+  const [alert, setAlert] = useState({
+    message: null,
+    open: false,
+  });
 
   function PasswordAuthenticate() {
+    setRequest({ ...request, authenticating: true, message: 'Authenticating...' });
+    setAlert({ message: null, open: false });
+
     fetch(`${args.ServerURL}/api/authenticate`, {
       method: 'POST',
       credentials: 'include',
@@ -14,20 +25,22 @@ function AuthenticationModal(args)
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        'authorization': btoa(passwordRef.current.value),
+        'authorization': btoa(request.password),
       }),
     })
     .then(response => {
-      passwordRef.current.value = null;
+      setRequest({ authenticating: false, password: null, message: 'Authenticate' });
+
       if (response.status != 200) {
-        return messageRef.current.classList.remove('hidden');
+        args.authModalRef.current.querySelector('input').select();
+        return setAlert({ message: 'Incorrect Password', open: true });
       }
 
       return response.json();
     })
     .then(authorization => {
       if (authorization) {
-        messageRef.current.classList.add('hidden');
+        setAlert({ message: null, open: false });
         args.authModalRef.current.classList.add('hidden');
         args.FetchFiles();
       }
@@ -46,28 +59,27 @@ function AuthenticationModal(args)
           <div className='mb-6'>
             <label className='block text-gray-700 text-sm font-bold mb-2'>File Hub Password</label>
             <input
+              onInput={(event) => setRequest({ ...request, password: event.target.value })}
               className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
               placeholder='Password'
-              ref={passwordRef}
               type='password'
             />
           </div>
           <div className='flex'>
             <button
+              disabled={request.password == null || request.authenticating}
               onClick={PasswordAuthenticate}
               className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
               type='button'
             >
-              Authenticate&nbsp;&nbsp;
-              <i className='bi bi-arrow-right'></i>
+              {request.message}
             </button>
 
-            <div className='ml-6 mt-2 hidden' ref={messageRef}>
-              <span className='text-red-500 font-bold'>Incorrect password</span>
+            <div className={`ml-6 mt-2 ${!alert.open && 'hidden'}`}>
+              <span className='text-red-500 font-bold'>{alert.message}</span>
             </div>
           </div>
         </div>
-
       </div>
     </div>
   );
